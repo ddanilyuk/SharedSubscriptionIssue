d0840e447aed9b3614850709b16f54fa2471a00a
ddanilyuk <danis.danilyuk@gmail.com>
2024-10-22 21:17:02 +0300

    Initial Commit

---
+++ TestSharedSubscription/TestSharedSubscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSharedSubscriptionApp.swift
+//  TestSharedSubscription
+//
+//  Created by Denys Danyliuk on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSharedSubscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
