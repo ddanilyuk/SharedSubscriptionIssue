14b394aeea1266af80fd1e3de3c38055b1c0f22a
ddanilyuk <danis.danilyuk@gmail.com>
2024-10-22 21:21:05 +0300

    Add bug test case

--- TestSharedSubscriptionTests/TestSharedSubscriptionTests.swift
+++ TestSharedSubscriptionTests/TestSharedSubscriptionTests.swift
@@ -7,30 +7,20 @@
 
 import XCTest
 @testable import TestSharedSubscription
+import ComposableArchitecture
+import Combine
 
 final class TestSharedSubscriptionTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSharedSubscription() throws {
+        @Dependency(\.defaultAppStorage) var defaults
+        var cancellable: (any Cancellable)?
+        @Shared(.appStorage("testInt")) var testInt: Int = 0
+        var isUpdated = false
+        cancellable = $testInt.publisher.sink { _ in
+            isUpdated = true
         }
+        defaults.set(42, forKey: "randomDefaultsKey")
+        XCTAssert(!isUpdated)
+        _ = cancellable
     }
-
 }
